ad0badd40107135405f9b1f39390ed1bcfff60c5
Vasu Vanka <vanka.vasu@gmail.com>
2025-09-22 10:23:36 +0530

    Auto-generated commit

---
+++ browser/chunk-24EKOIS6.js
@@ -0,0 +1,1 @@
+import{Ga as t,Za as o,_a as r,ib as n}from"./chunk-DCNC2EXY.js";var c=class s{static \u0275fac=function(e){return new(e||s)};static \u0275cmp=t({type:s,selectors:[["app-resources"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),n(1,"resources works!"),r())},encapsulation:2})};export{c as Resources};
